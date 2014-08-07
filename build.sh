f4c92589e45e4f8f948fc30e98674023ac961611
Tom Rogers <tom.rogers@adp.com>
2014-08-07 16:00:59 -0400

    updated firefox support

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 
 docker build -t benschw/selenium-hub ./selenium-hub
 
-docker build -t benschw/selenium-node-firefox ./selenium-node-firefox
+docker build -t tomrogers3/selenium-node-firefox ./selenium-node-firefox
 
 docker build -t tomrogers3/selenium-node-chrome ./selenium-node-chrome
